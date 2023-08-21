1a94db4fe1827cc2daa580edd5d417b28fa993e9
나경호 <gangho324@gmail.com>
2023-08-21 22:18:01 +0900

    feat: Application 구현
    RacingStadium을 GameConfig에서 받아와서 GameRun 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.game.view.RacingStadium;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO 구현 진행
-    }
+	public static void main(String[] args) {
+		GameConfig gameConfig = new GameConfig();
+		RacingStadium racingStadium = gameConfig.racingStadium();
+		GameRun gameRun = new GameRun(racingStadium);
+		gameRun.run();
+	}
 }
