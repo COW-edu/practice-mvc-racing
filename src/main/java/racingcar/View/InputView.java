b190dee19d1094e59a4bc223aec2c0d24e3be39e
taewook02 <taewook02@mju.ac.kr>
2023-08-22 20:00:36 +0900

    feat: getInstance 메서드 추가

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -5,6 +5,16 @@ import java.util.List;
 import racingcar.View.Util.StringParser;
 
 public class InputView implements ViewInterface {
+  private static ViewInterface instance = null;
+
+  private InputView() {};
+
+  public static ViewInterface getInstance() {
+    if (instance == null)
+      instance = new InputView();
+    return instance;
+  }
+
   public List<String> InputCarName() {
     System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     return StringParser.ParseCarName(Console.readLine());
--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -1,5 +1,13 @@
 package racingcar.View;
 
 public class OutputView implements ViewInterface {
+  private static ViewInterface instance = null;
 
+  private OutputView() {};
+
+  public static ViewInterface getInstance() {
+    if (instance == null)
+      instance = new OutputView();
+    return instance;
+  }
 }
