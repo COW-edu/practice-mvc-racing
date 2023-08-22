2ff7d717f39c0dd36cd41b5e811dd155257e7634
나경호 <gangho324@gmail.com>
2023-08-22 20:25:34 +0900

    feat: 우승자 등록 및 목록 조회
    우승자 등록 및 목록 조회 메서드 생성

--- src/main/java/racingcar/game/service/GameServiceImpl.java
+++ src/main/java/racingcar/game/service/GameServiceImpl.java
@@ -1,5 +1,11 @@
 package racingcar.game.service;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import racingcar.car.entity.Car;
+import racingcar.game.entity.Winner;
 import racingcar.game.model.GameRepository;
 
 public class GameServiceImpl implements GameService {
@@ -18,5 +24,33 @@ public class GameServiceImpl implements GameService {
 	@Override
 	public int getRound() {
 		return gameRepository.getRoundCount();
+	}
+
+	@Override
+	public void saveWinner(List<Car> cars) {
+		gameRepository.saveWinners(checkWinner(cars));
+	}
+
+	@Override
+	public String getWinnerSheet() {
+		List<Winner> winners = gameRepository.findWinners();
+		return winners.stream()
+			.map(Winner::getName)
+			.collect(Collectors.joining(", "));
+	}
+
+	private List<Winner> checkWinner(List<Car> cars) {
+		return cars.stream()
+			.filter(car -> car.isWinner(car, getMaxPosition(cars)))
+			.map(Car::getName)
+			.map(Winner::new)
+			.collect(Collectors.toList());
+	}
+
+	private int getMaxPosition(List<Car> cars) {
+		Car anyWinnerCar = cars.stream()
+			.max(Comparator.comparingInt(Car::getPosition))
+			.get();
+		return anyWinnerCar.getPosition();
 	}
 }
