d5f650fc22f683ccdd265eb653fb19591b86d936
taewook02 <taewook02@mju.ac.kr>
2023-08-22 20:52:05 +0900

    feat: run 메서드 추가

--- src/main/java/racingcar/Controller/RaceSettingController.java
+++ src/main/java/racingcar/Controller/RaceSettingController.java
@@ -1,5 +1,11 @@
 package racingcar.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.Model.Domain.Car;
+import racingcar.Model.Domain.RaceRepository;
+import racingcar.View.InputView;
+
 public class RaceSettingController implements ControllerInterface {
   private static ControllerInterface instance = null;
 
@@ -13,6 +19,19 @@ public class RaceSettingController implements ControllerInterface {
 
   @Override
   public void run() {
+    RaceRepository raceRepository = RaceRepository.getInstance();
+    InputView inputView = InputView.getInstance();
 
+    List<String> carNameList = inputView.InputCarName();
+    int attemptTimes = inputView.InputAttemptTimes();
+
+    List<Car> carList = new ArrayList<>();
+    for (String s : carNameList) {
+      Car car = new Car(s);
+      carList.add(car);
+    }
+    raceRepository.addCar(carList);
+
+    raceRepository.saveAttemptTimes(attemptTimes);
   }
 }
