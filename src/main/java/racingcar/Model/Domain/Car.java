c21c041f8d581b6df9663e818cc461b6b24153c0
taewook02 <taewook02@mju.ac.kr>
2023-08-22 19:20:11 +0900

    refactor: 매직넘버 제거

--- src/main/java/racingcar/Model/Domain/Car.java
+++ src/main/java/racingcar/Model/Domain/Car.java
@@ -6,6 +6,9 @@ public class Car {
     private final String name;
     private int position = 0;
 
+    private final int MOVE_CONDITION = 4;
+    private final int MOVE_NUMBER = 1;
+
     public Car(String name) {
         this.name = name;
     }
@@ -20,7 +23,7 @@ public class Car {
 
     public void move() {
         int randomNumber = RandomNumberGenerator.generateRandomNumber();
-        if (randomNumber >= 4)
-            position += 1;
+        if (randomNumber >= MOVE_CONDITION)
+            position += MOVE_NUMBER;
     }
 }
