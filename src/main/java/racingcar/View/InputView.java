778ae33d47278fd5cf1e35cbaeae34ea07fe626f
taewook02 <taewook02@mju.ac.kr>
2023-08-22 20:27:44 +0900

    feat: 메서드 반환값 변경

--- src/main/java/racingcar/View/InputView.java
+++ src/main/java/racingcar/View/InputView.java
@@ -20,7 +20,7 @@ public class InputView implements ViewInterface {
     return StringParser.ParseCarName(Console.readLine());
   }
 
-  public Integer InputAttemptTimes() {
+  public int InputAttemptTimes() {
     System.out.println("시도할 회수는 몇회인가요?");
     return Integer.parseInt(Console.readLine());
   }
