5072c6b2d7d79529ce2c4d78349c869aff614ccc
taewook02 <taewook02@mju.ac.kr>
2023-08-22 20:02:36 +0900

    feat: initView 메서드 추가

--- src/main/java/racingcar/Controller/FrontController.java
+++ src/main/java/racingcar/Controller/FrontController.java
@@ -2,14 +2,19 @@ package racingcar.Controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import racingcar.View.InputView;
+import racingcar.View.OutputView;
+import racingcar.View.ViewInterface;
 
 public class FrontController {
   private static FrontController instance = null;
 
   private final Map<String, ControllerInterface> controllers = new HashMap<>();
+  private final Map<String, ViewInterface> views = new HashMap<>();
 
   private FrontController() {
     initControllers();
+    initViews();
   }
 
   public static FrontController getInstance() {
@@ -22,6 +27,11 @@ public class FrontController {
     controllers.put("RaceSetting", RaceSettingController.getInstance());
     controllers.put("RaceResult", RaceResultController.getInstance());
     controllers.put("RaceRun", RaceRunController.getInstance());
+  }
+
+  private void initViews() {
+    views.put("InputView", InputView.getInstance());
+    views.put("OutputView", OutputView.getInstance());
   }
 
   public void service(String message) {
