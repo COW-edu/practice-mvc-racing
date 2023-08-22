b266f68e6556c4d03ed64ea17da11ffb28368213
taewook02 <taewook02@mju.ac.kr>
2023-08-22 20:13:56 +0900

    feat: getInstance 메서드 추가

--- src/main/java/racingcar/Model/Domain/CarRepository.java
+++ src/main/java/racingcar/Model/Domain/CarRepository.java
@@ -4,5 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarRepository {
+  private static CarRepository instance = null;
+
   private List<Car> carList = new ArrayList<>();
+
+  private CarRepository() {}
+
+  public static CarRepository getInstance() {
+    if (instance == null)
+      instance = new CarRepository();
+    return instance;
+  }
 }
