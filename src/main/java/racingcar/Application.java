c9e31ae4651f6bdd519b426045df953ab92c336a
taewook02 <taewook02@mju.ac.kr>
2023-08-22 21:00:08 +0900

    feat: 메인 메서드 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.Controller.FrontController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        FrontController frontController = FrontController.getInstance();
+        frontController.service("RaceSetting");
     }
 }
