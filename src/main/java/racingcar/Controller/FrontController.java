83555c8d5f1c49a35b0a591e3ed43a2d31a6ea7f
taewook02 <taewook02@mju.ac.kr>
2023-08-22 19:41:46 +0900

    feat: getInstance 메서드 추가

--- src/main/java/racingcar/Controller/FrontController.java
+++ src/main/java/racingcar/Controller/FrontController.java
@@ -1,5 +1,19 @@
 package racingcar.Controller;
 
-public class FrontController {
+import java.util.HashMap;
+import java.util.Map;
 
+public class FrontController implements ControllerInterface {
+  private static ControllerInterface instance = null;
+
+  private final Map<String, Object> controllers = new HashMap<>();
+
+  private FrontController() {}
+
+  public static ControllerInterface getInstance() {
+    if (instance == null)
+      instance = new FrontController();
+    return instance;
+  }
+  
 }
