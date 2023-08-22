673842c0c0e4c08bb7684934b9feec027a9209d6
taewook02 <taewook02@mju.ac.kr>
2023-08-22 19:27:17 +0900

    feat: getInstance 메서드 추가

--- src/main/java/racingcar/Controller/RaceResultController.java
+++ src/main/java/racingcar/Controller/RaceResultController.java
@@ -1,5 +1,13 @@
 package racingcar.Controller;
 
 public class RaceResultController implements ControllerInterface {
+  private static ControllerInterface instance = null;
 
+  private RaceResultController() {}
+
+  public static ControllerInterface getInstance() {
+    if (instance == null)
+      instance = new RaceResultController();
+    return instance;
+  }
 }
