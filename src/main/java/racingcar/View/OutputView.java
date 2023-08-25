8b840c7a3a43c2b647d770df433626e0dda5193d
taewook02 <taewook02@mju.ac.kr>
2023-08-25 17:46:34 +0900

    feat: OutputView 메서드 추가

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -1,6 +1,22 @@
 package racingcar.View;
 
+import java.util.Map;
+
 public class OutputView implements ViewInterface {
+  public void outputRunTitle() {
+    System.out.println("실행 결과");
+  }
+  public void outputRunState(Map<String, Integer> raceState) {
+    for (Map.Entry<String, Integer> raceStatePair : raceState.entrySet()) {
+      System.out.print(raceStatePair.getKey());
+      System.out.print(" : ");
+      for (int i = 0; i < raceStatePair.getValue(); i++) {
+        System.out.print("-");
+      }
+      System.out.print("\n\n");
+    }
+  }
+
   @Override
   public void render() {
 
