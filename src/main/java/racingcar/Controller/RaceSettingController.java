e4fed2b0a3f3619125838d4c02544ff24ef298d1
taewook02 <taewook02@mju.ac.kr>
2023-08-25 16:33:55 +0900

    feat: 싱글톤 패턴 제거

--- src/main/java/racingcar/Controller/RaceSettingController.java
+++ src/main/java/racingcar/Controller/RaceSettingController.java
@@ -8,16 +8,6 @@ import racingcar.View.InputView;
 import racingcar.View.ViewInterface;
 
 public class RaceSettingController implements ControllerInterface {
-  private static ControllerInterface instance = null;
-
-  private RaceSettingController() {}
-
-  public static ControllerInterface getInstance() {
-    if (instance == null)
-      instance = new RaceSettingController();
-    return instance;
-  }
-
   @Override
   public void run() {
     RaceRepository raceRepository = RaceRepository.getInstance();
