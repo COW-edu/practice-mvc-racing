832e4e55ac9677bc1ce344a934f60bfbedf09b24
taewook02 <taewook02@mju.ac.kr>
2023-08-25 20:27:31 +0900

    feat: run 메소드 수정

--- src/main/java/racingcar/Model/Service/RaceResultCalculator.java
+++ src/main/java/racingcar/Model/Service/RaceResultCalculator.java
@@ -1,6 +1,11 @@
 package racingcar.Model.Service;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import racingcar.Model.Domain.RaceRepository;
+import racingcar.View.OutputView;
 import racingcar.View.ViewInterface;
 
 public class RaceResultCalculator implements ServiceInterface {
@@ -14,6 +19,12 @@ public class RaceResultCalculator implements ServiceInterface {
 
   @Override
   public void run() {
-
+    Map<String, Integer> raceState = raceRepository.getRaceState();
+    int maxValue = Collections.max(raceState.values());
+    List<String> maxKeys = raceState.entrySet().stream()
+        .filter(entry -> entry.getValue() == maxValue)
+        .map(Map.Entry::getKey)
+        .collect(Collectors.toList());
+    ((OutputView)outputView).outputResult(maxKeys);
   }
 }
