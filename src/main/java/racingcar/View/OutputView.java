c5c5b7497de90a5bc879f4a4ff1fc89f2385e201
taewook02 <taewook02@mju.ac.kr>
2023-08-25 16:38:06 +0900

    feat: 싱글톤 패턴 제거

--- src/main/java/racingcar/View/OutputView.java
+++ src/main/java/racingcar/View/OutputView.java
@@ -1,16 +1,6 @@
 package racingcar.View;
 
 public class OutputView implements ViewInterface {
-  private static ViewInterface instance = null;
-
-  private OutputView() {};
-
-  public static ViewInterface getInstance() {
-    if (instance == null)
-      instance = new OutputView();
-    return instance;
-  }
-
   @Override
   public void render() {
 
