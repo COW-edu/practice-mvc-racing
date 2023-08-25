071d301dbb86e78af85e2c56642f2317baef5b49
taewook02 <taewook02@mju.ac.kr>
2023-08-25 17:11:07 +0900

    feat: 불필요한 코드 제거

--- src/main/java/racingcar/Controller/RaceSettingController.java
+++ src/main/java/racingcar/Controller/RaceSettingController.java
@@ -1,12 +1,8 @@
 package racingcar.Controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import racingcar.Model.Domain.Car;
 import racingcar.Model.Domain.RaceRepository;
 import racingcar.Model.Service.RaceSetter;
 import racingcar.Model.Service.ServiceInterface;
-import racingcar.View.InputView;
 import racingcar.View.ViewInterface;
 
 public class RaceSettingController implements ControllerInterface {
