1aaca1d11c48c329ca0769bf8332f37f03e87885
taewook02 <taewook02@mju.ac.kr>
2023-08-25 17:10:37 +0900

    feat: 컨트롤러 로직을 서비스로 이동

--- src/main/java/racingcar/Controller/RaceSettingController.java
+++ src/main/java/racingcar/Controller/RaceSettingController.java
@@ -4,30 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 import racingcar.Model.Domain.Car;
 import racingcar.Model.Domain.RaceRepository;
+import racingcar.Model.Service.RaceSetter;
+import racingcar.Model.Service.ServiceInterface;
 import racingcar.View.InputView;
 import racingcar.View.ViewInterface;
 
 public class RaceSettingController implements ControllerInterface {
   private final RaceRepository raceRepository;
   private final ViewInterface inputView;
+  private final ServiceInterface raceSetter;
 
   public RaceSettingController(RaceRepository raceRepository, ViewInterface inputView) {
     this.raceRepository = raceRepository;
     this.inputView = inputView;
+    raceSetter = new RaceSetter(raceRepository, inputView);
   }
 
   @Override
   public void run() {
-    List<String> carNameList = ((InputView)inputView).InputCarName();
-    int attemptTimes = ((InputView)inputView).InputAttemptTimes();
-
-    List<Car> carList = new ArrayList<>();
-    for (String s : carNameList) {
-      Car car = new Car(s);
-      carList.add(car);
-    }
-    raceRepository.addCar(carList);
-
-    raceRepository.saveAttemptTimes(attemptTimes);
+    raceSetter.run();
   }
 }
--- src/main/java/racingcar/Model/Service/RaceSetter.java
+++ src/main/java/racingcar/Model/Service/RaceSetter.java
@@ -1,8 +1,33 @@
 package racingcar.Model.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import racingcar.Model.Domain.Car;
+import racingcar.Model.Domain.RaceRepository;
+import racingcar.View.InputView;
+import racingcar.View.ViewInterface;
+
 public class RaceSetter implements ServiceInterface {
+  private final RaceRepository raceRepository;
+  private final ViewInterface inputView;
+
+  public RaceSetter(RaceRepository raceRepository, ViewInterface inputView) {
+    this.raceRepository = raceRepository;
+    this.inputView = inputView;
+  }
+
   @Override
   public void run() {
+    List<String> carNameList = ((InputView)inputView).InputCarName();
+    int attemptTimes = ((InputView)inputView).InputAttemptTimes();
 
+    List<Car> carList = new ArrayList<>();
+    for (String s : carNameList) {
+      Car car = new Car(s);
+      carList.add(car);
+    }
+    raceRepository.addCar(carList);
+
+    raceRepository.saveAttemptTimes(attemptTimes);
   }
 }
