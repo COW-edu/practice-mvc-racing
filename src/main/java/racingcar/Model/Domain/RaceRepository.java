6dbb21dd88edae37a2cc0bd0093d6871f344d407
taewook02 <taewook02@mju.ac.kr>
2023-08-25 16:49:13 +0900

    feat: 싱글톤 패턴 제거

--- src/main/java/racingcar/Model/Domain/RaceRepository.java
+++ src/main/java/racingcar/Model/Domain/RaceRepository.java
@@ -4,20 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RaceRepository {
-  private static RaceRepository instance = null;
-
   private List<Car> carList = new ArrayList<>();
   private int carNumber = 0;
 
   private int attemptTimes;
-
-  private RaceRepository() {}
-
-  public static RaceRepository getInstance() {
-    if (instance == null)
-      instance = new RaceRepository();
-    return instance;
-  }
 
   public void addCar(Car car) {
     carList.add(car);
