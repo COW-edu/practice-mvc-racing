feeee14403aed8097786fd320dc91cf292682974
나경호 <gangho324@gmail.com>
2023-08-27 01:26:13 +0900

    refactor: FrontController 제어
    GameManger에 FrontController를 외부에서 주입

--- src/main/java/racingcar/GameConfig.java
+++ src/main/java/racingcar/GameConfig.java
@@ -1,20 +1,9 @@
 package racingcar;
 
-import racingcar.game.controller.GameController;
-import racingcar.game.controller.GameControllerImpl;
-import racingcar.game.model.GameRepository;
-import racingcar.game.model.GameRepositoryImpl;
-import racingcar.game.service.GameService;
-import racingcar.game.service.GameServiceImpl;
+import racingcar.common.front.FrontController;
 import racingcar.game.view.RacingStadium;
 import racingcar.manager.GameManger;
 import racingcar.manager.GameMangerImpl;
-import racingcar.car.controller.CarController;
-import racingcar.car.controller.CarControllerImpl;
-import racingcar.car.model.CarRepository;
-import racingcar.car.model.CarRepositoryImpl;
-import racingcar.car.service.CarService;
-import racingcar.car.service.CarServiceImpl;
 
 public class GameConfig {
 
@@ -23,31 +12,11 @@ public class GameConfig {
 	}
 
 	public GameManger gameManager() {
-		return new GameMangerImpl(gameController(), carController());
+		return new GameMangerImpl(frontController());
 	}
 
-	public GameController gameController() {
-		return new GameControllerImpl(gameService());
-	}
-
-	public GameService gameService() {
-		return new GameServiceImpl(gameRepository());
-	}
-
-	public GameRepository gameRepository() {
-		return new GameRepositoryImpl();
-	}
-
-	public CarController carController() {
-		return new CarControllerImpl(carService());
-	}
-
-	public CarService carService() {
-		return new CarServiceImpl(carRepository());
-	}
-
-	public CarRepository carRepository() {
-		return new CarRepositoryImpl();
+	public FrontController frontController() {
+		return new FrontController();
 	}
 
 }
