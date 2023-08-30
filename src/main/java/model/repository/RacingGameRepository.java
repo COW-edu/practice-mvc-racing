52b6e91630e68abb0f35f9c74041ed4f48bd5dac
koseonje <kosunje1455@navet.com>
2023-08-30 12:09:14 +0900

    RacingGame결과를 담는 컬렉션 생성

--- src/main/java/model/repository/RacingGameRepository.java
+++ src/main/java/model/repository/RacingGameRepository.java
@@ -6,8 +6,13 @@ import model.domain.Car;
 
 public class RacingGameRepository {
   private final List<Car> racingCars = new ArrayList<>();
+  private final List<String> racingProcess = new ArrayList<>();
 
   public List<Car> getRacingCars() {
     return racingCars;
+  }
+
+  public List<String> getRacingProcess() {
+    return racingProcess;
   }
 }
