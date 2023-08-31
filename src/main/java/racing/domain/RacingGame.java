5f8c70c02f49f5f5657be9a709fba3d5e7a4e5a6
koseonje <kosunje1455@navet.com>
2023-08-31 16:16:13 +0900

    필요없는 import문 제거

--- src/main/java/racing/domain/RacingGame.java
+++ src/main/java/racing/domain/RacingGame.java
@@ -1,12 +1,10 @@
 package racing.domain;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class RacingGame {
 
