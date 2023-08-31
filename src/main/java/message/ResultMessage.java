14d387ca429991c09ba6d26e4dd8fe84143d2889
koseonje <kosunje1455@navet.com>
2023-08-31 16:15:13 +0900

    결과 안내 메시지 클래스 추가

---
+++ src/main/java/message/ResultMessage.java
@@ -0,0 +1,8 @@
+package message;
+
+public class ResultMessage {
+
+  public final static String RACING_RESULT_INSTRUCTION = "실행 결과";
+
+  public final static String WINNER_INSTRUCTION = "최종 우승자 : ";
+}
