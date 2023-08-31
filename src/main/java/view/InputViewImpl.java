516afa3fd1f339b49a56f347792be17401be07bf
koseonje <kosunje1455@navet.com>
2023-08-31 11:53:16 +0900

    InputViewImpl 상수 처리 및 요청 메시지 추가

--- src/main/java/view/InputViewImpl.java
+++ src/main/java/view/InputViewImpl.java
@@ -2,16 +2,19 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.StringTokenizer;
+import message.RequestMessage;
 
 public class InputViewImpl implements InputView{
 
   @Override
   public StringTokenizer inputCar() {
-    return new StringTokenizer(Console.readLine(),",");
+    System.out.println(RequestMessage.REQUEST_CARS);
+    return new StringTokenizer(Console.readLine(),RequestMessage.CARS_SEPARATOR);
   }
 
   @Override
   public int inputRound() {
+    System.out.println(RequestMessage.REQUEST_ROUND);
     return Integer.parseInt(Console.readLine());
   }
 }
