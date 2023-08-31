3c004ace01dce627045b27ce77c0a92402698d8b
koseonje <kosunje1455@navet.com>
2023-08-31 16:04:21 +0900

    매직넘버 처리하기

--- src/main/java/racing/domain/Car.java
+++ src/main/java/racing/domain/Car.java
@@ -1,11 +1,18 @@
 package racing.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Car {
 
   private final String name;
   private int position = 0;
+  private final int START_RANDOM_NUMBER = 0;
+  private final int FINISH_RANDOM_NUMBER = 9;
+  private final int CHECK_NUMBER = 4;
+  private final String MOVE_FORWARD_CHECKPOINT = "-";
+
 
   public Car(String name) {
     this.name = name;
@@ -18,16 +25,16 @@ public class Car {
   }
 
   public void moveForward() {
-    if (Randoms.pickNumberInRange(0, 9) >= 4) {
+    if (Randoms.pickNumberInRange(START_RANDOM_NUMBER, FINISH_RANDOM_NUMBER) >= CHECK_NUMBER) {
       position++;
     }
   }
 
   public StringBuilder raceResult() {
     StringBuilder racingResult = new StringBuilder(this.name + " : ");
-    for (int i = 0; i < position; i++) {
-      racingResult.append("-");
-    }
+    racingResult.append(IntStream.range(0, position)
+        .mapToObj(i -> MOVE_FORWARD_CHECKPOINT)
+        .collect(Collectors.joining()));
     return racingResult;
   }
 
