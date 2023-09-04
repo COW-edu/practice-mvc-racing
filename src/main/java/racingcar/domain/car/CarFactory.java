e47a6671469a18d6e5a6c241de63e5d49f1496e8
Gopistol <rhrjs0131@naver.com>
2023-09-04 15:52:02 +0900

    refactor: 생성자 제한 기능 추가

--- src/main/java/racingcar/domain/car/CarFactory.java
+++ src/main/java/racingcar/domain/car/CarFactory.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 import racingcar.domain.car.Car;
 
 public class CarFactory {
+  private CarFactory() {}
 
   public static List<Car> createCar(List<String> carNames) {
     return carNames.stream().map(Car::new).collect(Collectors.toList());
